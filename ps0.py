e6f7e3551371f74e049661b7adb5b3107f36a12e
LManzhula <lida.manzhula@gmail.com>
2017-04-14 16:30:50 +0300

    Add files via upload

---
+++ ps0.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 03 16:44:42 2017
+
+@author: lmanzhula
+"""
+
+
+#print('Enter number x:')
+x=int(input('Enter number x:'))
+y=int(input('Enter number y:'))
+output_degree = x**y
+print('Enter number y:', str(output_degree))
+#print('X**y =')
+#print('log(x) =')
